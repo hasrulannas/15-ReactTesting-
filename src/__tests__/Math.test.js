00b0baa640d33ebb8ab185d42be17432a2325339
Hasrul Annas <hasrulans123@gmail.com>
2022-03-20 23:37:54 +0700

    react testing

--- src/__tests__/Math.test.js
+++ src/__tests__/Math.test.js
@@ -1,33 +1,32 @@
-import { Add, Substract, Multiplication } from '../Math'
+import { Add, Substract, Multiplication } from "../Math";
 
-test('Return sum of two arguments', () => {
+
+test("Return sum of two arguments", () => {
   // Arrange
-
+  const value1 = 2;
+  const value2 = 1;
   // Act
-
-
+  const result = Add(value1, value2);
   // Assert
+  expect(result).toBe(3);
+});
 
-})
-
-test('Return substraction of a - b', () => {
+test("Return substraction of a - b", () => {
   // Arrange
-
-
+  const value1 = 2;
+  const value2 = 1;
   // Act
-
-
+  const result = Substract(value1, value2);
   // Assert
+  expect(result).toBe(1);
+});
 
-})
-
-test('Return multiplication of two arguments', () => {
+test("Return multiplication of two arguments", () => {
   // Arrange
-
-
+  const value1 = 2;
+  const value2 = 1;
   // Act
-
-
+  const result = Multiplication(value1, value2);
   // Assert
-
-})
+  expect(result).toBe(2);
+});
